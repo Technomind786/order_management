88439269c66f23a0b98e5cbd6ea1bfdfba136f5a
Ammar Rampurawala <ammar.rampurawala@gmail.com>
2026-02-11 21:03:19 +0530

    Fixed indentation error

--- app.py
+++ app.py
@@ -508,10 +508,5 @@ if __name__ == "__main__":
         db.create_all()
         create_default_users()
 
-    if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        create_default_users()
-
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
 
