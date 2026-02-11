8693d98022f34c1816c6f37adba5f303929dc666
Ammar Rampurawala <ammar.rampurawala@gmail.com>
2026-02-11 20:25:22 +0530

    Initial OMS upload

--- app.py
+++ app.py
@@ -508,4 +508,10 @@ if __name__ == "__main__":
         db.create_all()
         create_default_users()
 
-    app.run(debug=True)
+    if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        create_default_users()
+
+    app.run(host="0.0.0.0", port=10000)
+
